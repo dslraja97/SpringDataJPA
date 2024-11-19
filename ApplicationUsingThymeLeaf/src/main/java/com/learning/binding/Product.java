d39d3f6dc938393797f0ee5d4f36e5e59aaca749
dslraja <dslraja@yahoo.com>
2024-11-19 17:28:27 +0530

    Form Validation code has updated using ThymeLeaf.

--- ApplicationUsingThymeLeaf/src/main/java/com/learning/binding/Product.java
+++ ApplicationUsingThymeLeaf/src/main/java/com/learning/binding/Product.java
@@ -1,9 +1,15 @@
 package com.learning.binding;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class Product {
 
+	@NotNull(message = "ProductId is reqired!")
 	private Integer pId;
+	@NotBlank(message = "Name is reqired!")
 	private String name;
+	@NotNull(message = "Price is reqired!")
 	private Long price;
 
 	public Integer getpId() {
--- ApplicationUsingThymeLeaf/src/main/java/com/learning/controller/ProductController.java
+++ ApplicationUsingThymeLeaf/src/main/java/com/learning/controller/ProductController.java
@@ -2,10 +2,13 @@ package com.learning.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.learning.binding.Product;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ProductController {
@@ -17,9 +20,13 @@ public class ProductController {
 	}
 
 	@PostMapping("/product")
-	public String saveProduct(Product p, Model model) {
+	public String saveProduct(@Valid Product p, BindingResult result, Model model) {
 		System.out.println(p);
-		model.addAttribute("message", "Product Saved Succesfully!!");
+		if (result.hasErrors()) {
+			return "index";
+		} else {
+			model.addAttribute("message", "Product Saved Succesfully!!");
+		}
 		return "index";
 	}
 }
